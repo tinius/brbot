22e7808ab2d71c746a26c20d4935368dcf379222
Niko Kommenda <niko.kommenda@washpost.com>
2025-11-16 23:34:50 +0000

    use gist for state management

--- check.js
+++ check.js
@@ -44,12 +44,47 @@ const format = (o) => {
 const urlSuffix =
   "&time_slot=18:00&party_size=2&halo_size_interval=24&start_date=11-26-2025&num_days=28&channel=SEVENROOMS_WIDGET&selected_lang_code=en";
 
-console.log(process.env);
+//console.log(process.env);
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
+const gistId = process.env.GIST_ID;
+const token = process.env.GIST_TOKEN;
+const gistUrl = `https://api.github.com/gists/${gistId}`;
+
+async function getState() {
+  const res = await fetch(gistUrl, {
+    headers: { Authorization: `token ${token}` },
+  });
+  if (!res.ok) throw new Error(`Failed to fetch gist: ${res.status}`);
+  const gist = await res.json();
+  const file = gist.files["br_data.json"];
+  return JSON.parse(file.content);
+}
+
+async function setState(newState) {
+  const body = {
+    files: {
+      "br_data.json": { content: JSON.stringify(newState, null, 2) },
+    },
+  };
+  const res = await fetch(gistUrl, {
+    method: "PATCH",
+    headers: {
+      Authorization: `token ${token}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+  if (!res.ok)
+    throw new Error(
+      `Failed to update gist: ${res.status}, ${await res.text()}`
+    );
+}
+
 const baseUrl = process.env.URL;
 const emailTo = process.env.EMAIL_TO;
+const emailFrom = process.env.EMAIL_FROM;
 const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
 
 const url = `${baseUrl}${urlSuffix}`;
@@ -60,11 +95,14 @@ sgMail.setApiKey(SENDGRID_API_KEY);
 
 async function main() {
   try {
+    const oldState = await getState();
+    // console.log(`Old state: ${JSON.stringify(oldState)}`);
+
     const res = await fetch(url);
     if (!res.ok) throw new Error(`Fetch failed: ${res.statusText}`);
     const data = await res.json();
 
-    console.log(data);
+    //console.log(data);
 
     const dates = Object.keys(data.data.availability);
 
@@ -98,16 +136,19 @@ async function main() {
       };
     });
 
+    console.log("-----");
     console.log(available);
+    console.log(oldState.available);
+    console.log("-----");
 
-    if (available.length > 0) {
+    if (available.length > 0 && !_.isEqual(available, oldState.available)) {
       console.log("Sending email...");
 
       const code = Math.random().toString(36).substring(2, 7).toUpperCase();
 
       const msg = {
         to: emailTo,
-        from: "nkommenda@hotmail.com", // can be any verified sender in SendGrid
+        from: emailFrom, // can be any verified sender in SendGrid
         subject: `${getSubject(available)} (#${code})`,
         html: `
       
@@ -127,6 +168,8 @@ async function main() {
       await sgMail.send(msg);
       console.log("Email sent!");
     }
+
+    await setState({ time: new Date(), available });
   } catch (err) {
     console.error("Error:", err);
   }
